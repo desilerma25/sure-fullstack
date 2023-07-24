d5e75c92a80d8a684797cb5f165b908dd7769d99
Desiree Lerma <desi.lerma96@yahoo.com>
2023-07-24 02:14:36 -0500

    desi-fullstack-01: Added ran necessary installs per README. Added function to handle form data that will be sent from the FE. Only allows POST.

--- intsureview_be/intsureview_be/apps/api/views.py
+++ intsureview_be/intsureview_be/apps/api/views.py
@@ -2,6 +2,24 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
 from intsureview_be.apps.api.serializers import UserSerializer, GroupSerializer
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+
+@require_http_methods(["POST"])
+def form_api(request):
+    if request.method == "POST":
+        try:
+            name_input = request.POST.get("nameInput")
+            select_input = request.POST.get("selectInput")
+            radio_input = request.POST.get("radioInput")
+            phone_input = request.POST.get("phoneInput")
+            email_input = request.POST.get("emailInput")
+
+            return JsonResponse({"message": "Form data received successfully!"})
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+
+    return JsonResponse({"error": "Method not allowed"}, status=405)
 
 
 class UserViewSet(viewsets.ModelViewSet):
